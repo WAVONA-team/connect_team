c6c0db20d4b9cd291ea43d6d658ffd5f6136a936
Vo7kov <jegor.volkov.a@gmail.com>
2023-12-02 22:28:15 +0100

    feat(TAS-33): add primitive layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
-import { getServerAuthSession } from "@/server/auth";
+import React from "react";
 
-export default async function Home() {
-  const session = await getServerAuthSession();
-
+const Home: React.FC = React.memo(() => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 "></div>
-    </main>
+    <>
+      <header>header</header>
+
+      <main className="flex-1">
+        <div className="m-auto w-1/2">
+          <p>home</p>
+        </div>
+      </main>
+
+      <footer>footer</footer>
+    </>
   );
-}
+});
+
+export default Home;
