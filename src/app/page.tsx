8fac85a19aea97cd3d55fa5253df79a55c4a7520
Vo7kov <jegor.volkov.a@gmail.com>
2023-12-05 19:19:28 +0100

    fix(TAS-33): refactor main layout & remove some unnessecary components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,23 +2,9 @@ import React from 'react';
 
 const Home: React.FC = React.memo(() => {
   return (
-    <>
-      <header>
-        header
-      </header>
-
-      <main className="flex-1">
-        <div className="w-1/2 m-auto">
-          <p>
-            home
-          </p>
-        </div>
-      </main>
-
-      <footer>
-        footer
-      </footer>
-    </>
+    <section>
+      home
+    </section>
   );
 });
 
