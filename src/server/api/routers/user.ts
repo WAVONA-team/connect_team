0cea87ed75ecfec9208d56f938c6fc7d0d8853dc
Jegor Volkov <79021164+Vo7kov@users.noreply.github.com>
2023-12-10 22:10:37 +0100

    feat(TAS-28): user api-routes added
    * feat(TAS-28): user api-routes added
    
    * fix(TAS-28): remove optional inputs
    
    * fix(TAS-28): add userRouter to rootRouter and change the way of editing methods in  db by userRouter
    
    * refactor(TAS-28): small refactoring

---
+++ src/server/api/routers/user.ts
@@ -0,0 +1,66 @@
+import { z } from 'zod';
+import {
+  createTRPCRouter,
+  protectedProcedure,
+  publicProcedure,
+} from '../trpc';
+
+export const userRouter = createTRPCRouter({
+  create: publicProcedure
+    .input(
+      z.object({
+        email: z.string().trim().email(),
+        name: z.string().trim(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      return ctx.db.user.create({
+        data: {
+          email: input.email,
+          name: input.name,
+        },
+      });
+    }),
+
+  delete: protectedProcedure
+    .input(z.object({
+      id: z.string().trim(),
+    }))
+    .mutation(async ({ ctx, input }) => {
+      return ctx.db.user.delete({
+        where: {
+          id: input.id,
+        },
+      });
+    }),
+
+  update: protectedProcedure
+    .input(z.object({
+      email: z.string().trim().email(),
+      name: z.string().trim(),
+      id: z.string().trim(),
+    }))
+    .mutation(async ({ ctx, input }) => {
+      return ctx.db.user.update({
+        where: {
+          id: input.id,
+        },
+        data: {
+          email: input.email,
+          name: input.name,
+        },
+      });
+    }),
+
+  get: publicProcedure
+    .input(z.object({
+      id: z.string().trim(),
+    }))
+    .query(async ({ ctx, input }) => {
+      return ctx.db.user.findUnique({
+        where: {
+          id: input.id,
+        },
+      });
+    }),
+});
