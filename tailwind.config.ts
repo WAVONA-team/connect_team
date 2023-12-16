936454e19caf2e7544647c2b1828ed5fb33bfcfe
Jegor Volkov <79021164+Vo7kov@users.noreply.github.com>
2023-12-16 21:57:54 +0100

    build(adding-colors): add colors to tailwing config (#17)

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,4 @@
 import { type Config } from "tailwindcss";
-import { fontFamily } from "tailwindcss/defaultTheme";
 
 export default {
   content: ["./src/**/*.tsx"],
@@ -12,6 +11,36 @@ export default {
       colors: {
         gray: "#1F1F1F",
         "gray-1": "#333",
+        primary: {
+          "button-fill": "#6366F1",
+          "majorelle-blue": "#4F46E5",
+          "palatinate-blue": "#4338CA",
+          "powder-blue": "#A5B4FC",
+        },
+        secondary: {
+          "cadet-grey": "#A1A1AA",
+          "rich-black": "#0E0D1F",
+        },
+        accent: {
+          "spring-bud": "#A3E635",
+          "green-yellow": "#BEF264",
+        },
+        background: {
+          "night": "#100E16",
+        },
+        surface: {
+          "raisin-black": "#1C1825",
+        },
+        onPrimary: {
+          "anti-flash-withe": "#F4F4F5",
+          "lavender": "#EBEFFF",
+        },
+        success: {
+          "dark-pastel-green": "#22C55E",
+        },
+        error: {
+          "imperial-red": "#EF4444",
+        },
       },
     },
     screens: {
