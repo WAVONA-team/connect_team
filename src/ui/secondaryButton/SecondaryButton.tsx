c4f2cd0a20b079f730ec51011af4f4b70ed05cd4
Vo7kov <jegor.volkov.a@gmail.com>
2023-12-18 21:11:33 +0100

    refactor(TAS-101): refactor secondaryButton

--- src/ui/secondaryButton/SecondaryButton.tsx
+++ src/ui/secondaryButton/SecondaryButton.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React from "react";
+import buttonClassName from "./helpers/buttonClassName";
 
 type Props = {
   text: string;
@@ -17,21 +18,7 @@ const SecondaryButton: React.FC<Props> = React.memo(
         disabled={disabled}
         className={`
           ${className}
-          bg-secondary-rich-black
-          rounded-lg
-          border
-          border-primary-button-fill
-          px-4
-          py-3.5
-          text-center
-          text-sm
-          text-primary-button-fill
-          outline-none
-          transition
-          hover:border-primary-majorelle-blue
-          hover:text-primary-majorelle-blue
-          active:border-primary-palatinate-blue
-          active:text-primary-palatinate-blue
+          ${buttonClassName}
           disabled:border-secondary-cadet-grey
           disabled:text-secondary-cadet-grey
         `}
