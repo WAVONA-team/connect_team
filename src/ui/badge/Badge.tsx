44e8fdf7c6ae443e8a150c7799d27e12ed83ed0b
Vo7kov <jegor.volkov.a@gmail.com>
2023-12-18 21:53:18 +0100

    feat(TAS-105): add new logic to badge

--- src/ui/badge/Badge.tsx
+++ src/ui/badge/Badge.tsx
@@ -5,10 +5,12 @@ type Props = {
   text: string;
   onClick: React.MouseEventHandler<HTMLButtonElement>;
   className?: string;
+  counterValue?: number;
+  withCloseButton?: boolean;
 };
 
 const Badge: React.FC<Props> = React.memo(
-  ({ text, onClick, className = "" }) => {
+  ({ text, onClick, className = "", counterValue, withCloseButton }) => {
     return (
       <button
         type="button"
@@ -19,6 +21,9 @@ const Badge: React.FC<Props> = React.memo(
           hover:text-accent-green-yellow
           active:border-accent-spring-bud
           active:text-accent-spring-bud
+          flex
+          items-center
+          gap-2
           rounded-2xl
           border
           border-white
@@ -29,7 +34,24 @@ const Badge: React.FC<Props> = React.memo(
           transition
         `}
       >
+        {counterValue && (
+          <div className="text-onAccent-pakistan-green bg-accent-green-yellow h-6 w-6 rounded-full">
+            {counterValue}
+          </div>
+        )}
         {text}
+        {withCloseButton && (
+          <div
+            className={`
+              h-4
+              w-4
+              bg-[url("/images/close.svg")]
+              bg-contain
+              bg-center
+              bg-no-repeat
+            `}
+          />
+        )}
       </button>
     );
   },
