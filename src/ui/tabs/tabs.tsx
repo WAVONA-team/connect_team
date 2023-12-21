3e8cb32fd52750a82a69e6887540db18f619660d
Артём <70152685+aguilam@users.noreply.github.com>
2023-12-21 20:04:40 +0400

    feat(TAS-102): added Tab-component (#26)
    * feat(TAS-102): added Tab-component
    
    * refactor(Tas-102)

---
+++ src/ui/tabs/tabs.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import Link from "next/link";
+type Props = {
+  isActive?: boolean;
+  isDisabled?: boolean;
+  link: string;
+  title: string;
+};
+
+const Tabs: React.FC<Props> = React.memo(
+  ({ isActive, isDisabled, link, title }) => (
+  <>
+    {isDisabled ? (
+      <p className=' text-center h-9 border-b-2 border-b-secondary-cadet-gray text-secondary-cadet-grey px-3 py-2'>
+        {title}
+      </p >
+    ) : (
+      <Link
+        href={link}
+        className={`
+          h-9 border-b-2 px-3 py-2 block text-center
+          ${isActive ? ' border-b-primary-majorelle-blue text-primary-majorelle-blue'
+          : 'border-b-zinc-300 text-zinc-300 hover:border-b-primary-powder-blue hover:text-primary-button-fill'
+          }`}
+      >
+        {title}
+      </Link>
+    )}
+  </>
+));
+
+export default Tabs;
