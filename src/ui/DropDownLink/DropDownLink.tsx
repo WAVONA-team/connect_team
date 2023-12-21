9c513717814966953dcc4947cdf432f1f15dcb78
aguilam <pexfex@mail.ru>
2023-12-21 18:14:24 +0400

    refactor(Tas-110)

---
+++ src/ui/DropDownLink/DropDownLink.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Image from "next/image";
+
+type Props = {
+  image: string;
+  isDisabled?: boolean;
+  link?: string;
+  title: string;
+  number?: number;
+};
+
+const DropDownLink: React.FC<Props> = React.memo(
+  ({ image, isDisabled, link, title, number }) => (
+  <>
+    {isDisabled ? (
+      <div className=' w-96 h-12 flex flex-row items-center gap-2 px-5 py-3 bg-secondary-cadet-grey text-onPrimary-lavender'>
+        <Image src={image} alt={title} width={36} height={36} className=" rounded-full "></Image>
+        <p>{title}</p>
+        <div className=" rounded-full bg-accent-green-yellow px-2 text-black">
+          <p>{number}</p>
+        </div>
+      </div>
+    ) : (
+      <a
+        href={link}
+        className=' w-96 h-12  flex flex-row items-center gap-2 px-5 py-3 bg-transparent text-onPrimary-anti-flash-withe hover:bg-primary-majorelle-blue active:bg-primary-button-fill'
+
+      >
+        <Image src={image} alt={title} width={36} height={36}></Image>
+        <p>{title}</p>
+        <div className=" rounded-full bg-accent-green-yellow px-2 text-black">
+          <p>{number}</p>
+        </div>
+      </a>
+    )}
+  </>
+));
+
+export default DropDownLink;
