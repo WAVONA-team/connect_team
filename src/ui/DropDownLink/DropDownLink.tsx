fab35d7bfac27a56dc56220a286060e127f10d79
VofSwords <47701851+VofSwords@users.noreply.github.com>
2023-12-22 20:51:42 +0500

    fix(tas-110): dropdown link logic fix

--- src/ui/DropDownLink/DropDownLink.tsx
+++ src/ui/DropDownLink/DropDownLink.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import  Link  from "next/link";
+import Link from "next/link";
 
 type Props = {
   image: string;
@@ -12,29 +12,35 @@ type Props = {
 
 const DropDownLink: React.FC<Props> = React.memo(
   ({ image, isDisabled, link, title, number }) => (
-  <>
-    {isDisabled ?? !link ? (
-      <div className=' w-96 h-12 flex flex-row items-center gap-2 px-5 py-3 bg-secondary-cadet-grey text-onPrimary-lavender'>
-        <Image src={image} alt={title} width={36} height={36} className=" rounded-full "></Image>
-        <p>{title}</p>
-        <div className=" rounded-full bg-accent-green-yellow px-2 text-black">
-          <p>{number}</p>
+    <>
+      {!link || isDisabled ? (
+        <div className=" flex h-12 w-96 flex-row items-center gap-2 bg-secondary-cadet-grey px-5 py-3 text-onPrimary-lavender">
+          <Image
+            src={image}
+            alt={title}
+            width={36}
+            height={36}
+            className=" rounded-full "
+          ></Image>
+          <p>{title}</p>
+          <div className=" rounded-full bg-accent-green-yellow px-2 text-black">
+            <p>{number}</p>
+          </div>
         </div>
-      </div>
-    ) : (
-      <Link
-        href={link}
-        className=' w-96 h-12  flex flex-row items-center gap-2 px-5 py-3 bg-transparent text-onPrimary-anti-flash-withe hover:bg-primary-majorelle-blue active:bg-primary-button-fill'
-
-      >
-        <Image src={image} alt={title} width={36} height={36}></Image>
-        <p>{title}</p>
-        <div className=" rounded-full bg-accent-green-yellow px-2 text-black">
-          <p>{number}</p>
-        </div>
-      </Link>
-    )}
-  </>
-));
+      ) : (
+        <Link
+          href={link}
+          className=" flex h-12  w-96 flex-row items-center gap-2 bg-transparent px-5 py-3 text-onPrimary-anti-flash-withe hover:bg-primary-majorelle-blue active:bg-primary-button-fill"
+        >
+          <Image src={image} alt={title} width={36} height={36}></Image>
+          <p>{title}</p>
+          <div className=" rounded-full bg-accent-green-yellow px-2 text-black">
+            <p>{number}</p>
+          </div>
+        </Link>
+      )}
+    </>
+  ),
+);
 
 export default DropDownLink;
