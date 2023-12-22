b0333b818225c368f4c479058b92ee2cc130f346
Vo7kov <jegor.volkov.a@gmail.com>
2023-12-22 15:43:11 +0200

    fix(TAS-134): fix logo

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Link from "next/link";
 
 const Logo: React.FC = React.memo(() => (
-  <Link className="flex items-end gap-4" href={"/"}>
+  <Link className="flex items-center gap-4" href={"/"}>
     <div className="h-8 w-8 rounded border border-white" />
 
     <p className="text-xl text-white leading-6">ConnectTeam</p>
