9dc14cb2e84fa28922a4b799e329329d8c87f461
Vo7kov <jegor.volkov.a@gmail.com>
2023-12-22 22:00:53 +0200

    fix(TAS-133): fix popOver

--- src/modules/notificationPopOver/NotificationPopOver.tsx
+++ src/modules/notificationPopOver/NotificationPopOver.tsx
@@ -2,7 +2,19 @@ import React from "react";
 import PopOver from "@/ui/popOver/PopOver";
 
 const NotificationPopOver: React.FC = React.memo(() => {
-  return <PopOver>notifications</PopOver>;
+  return (
+    <PopOver
+      button={
+        <div
+          className={`bg-[url("/images/bell.svg")] h-8 w-8 bg-contain bg-center bg-no-repeat`}
+        />
+      }
+      width="8"
+      height="8"
+    >
+      notifications
+    </PopOver>
+  );
 });
 
 export default NotificationPopOver;
