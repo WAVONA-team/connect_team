6c8fefc38b0e431e8cdb4723cab9fb03975c25c4
Vo7kov <jegor.volkov.a@gmail.com>
2023-12-27 23:05:23 +0200

    feat(TAS-126): fix Artem's dropdown link

---
+++ src/ui/DropDownItem/DropDownItem.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import classNames from "classnames";
+import Link from "next/link";
+
+import ItemClassNames from "./helpers/ItemClassNames";
+
+type Props = {
+  children: React.ReactNode;
+  width?: string;
+  className?: string;
+  link?: string;
+  isDisabled?: boolean;
+};
+
+const DropDownItem: React.FC<Props> = React.memo(
+  ({
+    children,
+    width = "full",
+    className = "",
+    link = "",
+    isDisabled = false,
+  }) => {
+    return !link || isDisabled ? (
+      <div
+        className={classNames(`
+          ${className}
+          ${ItemClassNames}
+          w-${width}
+        `,
+        {
+          "pointer-events-none bg-secondary-cadet-grey": isDisabled,
+        })}
+      >
+        {children}
+      </div>
+    ) : (
+      <Link
+        href={link}
+        className={classNames(`
+          ${className}
+          ${ItemClassNames}
+          w-${width}
+          active:bg-primary-neon-blue
+        `, {
+          "pointer-events-none bg-secondary-cadet-grey": isDisabled,
+        })}
+      >
+        {children}
+      </Link>
+    );
+  },
+);
+
+export default DropDownItem;
