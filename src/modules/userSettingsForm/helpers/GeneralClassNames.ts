1a5152a584e0515e6cd523c679e877b76171d066
Vo7kov <jegor.volkov.a@gmail.com>
2024-01-07 00:38:43 +0200

    feat(TAS-55): add userSettingsForm

---
+++ src/modules/userSettingsForm/helpers/GeneralClassNames.ts
@@ -0,0 +1,12 @@
+const generalClassNames = {
+  sectionWrapper: "flex flex-col gap-6",
+  sectionTitle: "text-xl font-bold text-onPrimary-anti-flash-withe",
+  sectionAdditionalWrapper: "flex max-w-4xl items-center justify-between",
+  sectionAdditionalText: "text-base text-secondary-cadet-grey",
+  label: "grid grid-cols-[1fr,2fr] items-center gap-x-28 max-w-4xl",
+  labelText: "mt-8 text-base text-onPrimary-anti-flash-withe",
+  inputWrapper: "flex flex-col gap-4",
+  labelInput: "",
+};
+
+export default generalClassNames;
