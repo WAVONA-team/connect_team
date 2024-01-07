18b64b1b5cc46b1d7a5fe542bc029dd9b9171c39
Vo7kov <jegor.volkov.a@gmail.com>
2024-01-07 14:55:49 +0200

    fix(TAS-55): add new fields to form and change some general classnames

--- src/modules/userSettingsForm/helpers/GeneralClassNames.ts
+++ src/modules/userSettingsForm/helpers/GeneralClassNames.ts
@@ -3,9 +3,11 @@ const generalClassNames = {
   sectionTitle: "text-xl font-bold text-onPrimary-anti-flash-withe",
   sectionAdditionalWrapper: "flex max-w-4xl items-center justify-between",
   sectionAdditionalText: "text-base text-secondary-cadet-grey",
-  label: "grid grid-cols-[1fr,2fr] items-center gap-x-28 max-w-4xl",
-  labelText: "mt-8 text-base text-onPrimary-anti-flash-withe",
   inputWrapper: "flex flex-col gap-4",
+  labelWrapper: "grid grid-cols-[2fr,1fr] gap-x-9",
+  label: "grid grid-cols-2 items-center",
+  labelAdditional: "flex items-center gap-4 mt-6",
+  labelText: "mt-8 text-base text-onPrimary-anti-flash-withe",
   labelInput: "",
 };
 
--- src/modules/userSettingsForm/types/InputsValue.ts
+++ src/modules/userSettingsForm/types/InputsValue.ts
@@ -1,10 +1,12 @@
 type InputsValue = {
   name: string;
   profession: string;
+  isVisibleForTeam: boolean;
   city: string;
   age: string;
   languages: string;
   email: string;
+  preferredTypeOfCommunication: string;
   telegram: string;
   discord: string;
   description: string;
