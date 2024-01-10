dabf4aa6a36209cf62f92ded1fc1e23a4b18199f
Vo7kov <jegor.volkov.a@gmail.com>
2024-01-10 15:22:36 +0200

    feat(TAS-150): icon init

---
+++ src/ui/icons/helpers/generalClassNames.ts
@@ -0,0 +1,6 @@
+const generalClassNames = {
+  fill: "fill-onPrimary-anti-flash-withe transition hover:fill-secondary-cadet-grey active:fill-accent-spring-bud",
+  stroke: "stroke-onPrimary-anti-flash-withe transition hover:stroke-secondary-cadet-grey active:stroke-accent-spring-bud"
+};
+
+export default generalClassNames;
