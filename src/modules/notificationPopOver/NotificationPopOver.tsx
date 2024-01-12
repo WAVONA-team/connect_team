28c77666f51729a31f609b06ce1ef806007b287a
Vo7kov <jegor.volkov.a@gmail.com>
2024-01-12 14:40:25 +0200

    fix(MASTER): fix header icon

--- src/modules/notificationPopOver/NotificationPopOver.tsx
+++ src/modules/notificationPopOver/NotificationPopOver.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import PopOver from "@/ui/popOver/PopOver";
+import BellIcon from "@/ui/icons/bell/Bell";
 
 const NotificationPopOver: React.FC = React.memo(() => {
   return (
     <PopOver
       button={
-        <div
-          className={`bg-[url("/images/bell.svg")] h-8 w-8 bg-contain bg-center bg-no-repeat`}
-        />
+        <button className="h-8 w-8">
+          <BellIcon />
+        </button>
       }
       width="8"
       height="8"
