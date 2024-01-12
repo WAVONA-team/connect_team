e61d0883f8d8778665fd78503c3985702a082b45
aguilam <pexfex@mail.ru>
2024-01-12 15:49:27 +0400

    feat(Tas-143): added Logo to header

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,11 +1,11 @@
 import React from "react";
 import Link from "next/link";
-
+import Image from "next/image";
+import logo from '../../../public/images/logo.svg';
 const Logo: React.FC = React.memo(() => (
   <Link className="flex items-center gap-4" href={"/"}>
-    <div className="h-8 w-8 rounded border border-white" />
-
-    <p className="text-xl text-white leading-6">ConnectTeam</p>
+    <Image src={logo as string} alt="logo" width={50} height={50} />
+    <p className="text-xl text-white leading-6 font-bold">ConnectTeam</p>
   </Link>
 ));
 
