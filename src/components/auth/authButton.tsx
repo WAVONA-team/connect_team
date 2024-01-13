2c5f1ee07e6b5505c9c50c9abc9b82d0058c7e5d
Vo7kov <jegor.volkov.a@gmail.com>
2024-01-13 23:33:01 +0200

    fix(TAS-142): modified auth button

--- src/components/auth/authButton.tsx
+++ src/components/auth/authButton.tsx
@@ -1,8 +1,8 @@
 "use client";
+import React from "react";
 import { signIn } from "next-auth/react";
+
 import Image from "next/image";
-import { type FC } from "react";
-import { useSearchParams } from "next/navigation";
 
 type Props = {
   image: HTMLImageElement;
@@ -10,24 +10,20 @@ type Props = {
   provider: "google" | "github" | "vk";
 };
 
-const AuthButton: FC<Props> = ({ image, text, provider }) => {
-  const searchParams = useSearchParams();
-  const callbackUrl = searchParams.get("callbackUrl") ?? "/";
-  console.log(callbackUrl);
+const AuthButton: React.FC<Props> = ({ image, text, provider }) => {
   const handleClick = async () => {
-    await signIn(provider, { callbackUrl });
+    await signIn(provider, { callbackUrl: `/pages/projects` });
   };
 
   return (
-    <>
-      <div
-        onClick={handleClick}
-        className="justify-left flex cursor-pointer items-center gap-3 rounded-xl bg-gray-1 px-16 py-5 hover:bg-neutral-700"
-      >
-        <Image src={image} alt={text} />
-        <p className="font-inter text-xl font-normal text-white">{text}</p>
-      </div>
-    </>
+    <button
+      type="button"
+      onClick={handleClick}
+      className="justify-left bg-gray-1 flex cursor-pointer items-center gap-3 rounded-xl px-16 py-5 hover:bg-neutral-700"
+    >
+      <Image src={image} alt={text} />
+      <p className="font-inter text-xl font-normal text-white">{text}</p>
+    </button>
   );
 };
 export default AuthButton;
