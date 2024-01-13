ca32efb2e14d9eb91d6ada15381c6c172adbf35f
Vo7kov <jegor.volkov.a@gmail.com>
2024-01-13 19:39:02 +0200

    feat(TAS-157): add navBar and fix tabs

---
+++ src/components/navBar/NavBar.tsx
@@ -0,0 +1,33 @@
+"use client";
+import React from "react";
+import { usePathname } from "next/navigation";
+import { useSession } from "next-auth/react";
+
+import Tabs from "@/ui/tabs/tabs";
+
+const NavBar: React.FC = React.memo(() => {
+  const pathName = usePathname();
+  const session = useSession();
+
+  return (
+    <nav className="relative">
+      <div className="flex">
+        <Tabs
+          link="/projects"
+          title="Проекты"
+          isActive={pathName === "/projects"}
+        />
+
+        <Tabs
+          link={`/responses/${session.data?.user.id}`}
+          title="Отклики"
+          isActive={pathName === `/responses/${session.data?.user.id}`}
+        />
+      </div>
+
+      <div className="absolute bottom-0 -z-10 w-full border border-onPrimary-anti-flash-withe" />
+    </nav>
+  );
+});
+
+export default NavBar;
--- src/ui/tabs/tabs.tsx
+++ src/ui/tabs/tabs.tsx
@@ -1,5 +1,7 @@
 import React from "react";
+import classNames from "classnames";
 import Link from "next/link";
+
 type Props = {
   isActive?: boolean;
   isDisabled?: boolean;
@@ -9,24 +11,26 @@ type Props = {
 
 const Tabs: React.FC<Props> = React.memo(
   ({ isActive, isDisabled, link, title }) => (
-  <>
-    {isDisabled ? (
-      <p className=' text-center h-9 border-b-2 border-b-secondary-cadet-gray text-secondary-cadet-grey px-3 py-2'>
-        {title}
-      </p >
-    ) : (
-      <Link
-        href={link}
-        className={`
-          h-9 border-b-2 px-3 py-2 block text-center
-          ${isActive ? ' border-b-primary-majorelle-blue text-primary-majorelle-blue'
-          : 'border-b-zinc-300 text-zinc-300 hover:border-b-primary-powder-blue hover:text-primary-neon-blue'
-          }`}
-      >
-        {title}
-      </Link>
-    )}
-  </>
-));
+    <>
+      {isDisabled ? (
+        <p className="border-b-secondary-cadet-gray h-9 border-b-2 px-3 py-2 text-center text-secondary-cadet-grey">
+          {title}
+        </p>
+      ) : (
+        <Link
+          href={link}
+          className={classNames("block border-b-2 px-3 py-4 text-center", {
+            "border-b-primary-majorelle-blue text-primary-majorelle-blue":
+              isActive,
+            "border-b-zinc-300 text-zinc-300 hover:border-b-primary-powder-blue hover:text-primary-neon-blue":
+              !isActive,
+          })}
+        >
+          {title}
+        </Link>
+      )}
+    </>
+  ),
+);
 
 export default Tabs;
