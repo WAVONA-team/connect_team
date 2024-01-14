72453675ad4cb45b7f7899af8f586acaedd9da2c
aguilam <pexfex@mail.ru>
2024-01-14 15:52:22 +0400

    feat(Tas-145): updated user page

--- src/app/pages/user/[id]/page.tsx
+++ src/app/pages/user/[id]/page.tsx
@@ -5,7 +5,14 @@ import Link from "next/link";
 import { api } from "@/trpc/server";
 
 import Container from "@/ui/container/Container";
+import SectionWrapper from "@/ui/sectionWrapper/SectionWrapper";
+import Badge from "@/ui/badge/Badge";
 
+import NavBar from "@/components/navBar/NavBar";
+import MainButtonLink from "@/ui/mainButton/MainButtonLink";
+import ProfileImage from "@/ui/profileImage/ProfileImage";
+import arrowLeft from "../../../../../public/images/arrowLeft.svg";
+import UserProjectCard from "../../../../modules/userProjectCard/UserProjectCard"
 interface Props {
   params: {
     id: string;
@@ -13,148 +20,83 @@ interface Props {
 }
 
 const User: React.FC<Props> = async ({ params }) => {
-
   const user = await api.user.get.query({ id: params.id });
+  if (!user) {
+    return <p>error</p>;
+  }
 
   return (
     <Container>
-      <nav className="mb-6 w-full border-b-2 border-gray-400">
-        <div className=" flex w-3/12 flex-row justify-between">
-          <a
-            href={`/projects`}
-            className="font-['Inter'] text-base font-medium leading-tight text-gray-500"
-          >
-            Проекты
-          </a>
-          <a
-            href=""
-            className="border-b-4 font-['Inter'] text-base font-medium leading-tight text-zinc-100"
-          >
-            Профиль
-          </a>
-          <a
-            href={`/responses/${params.id}`}
-            className="font-['Inter'] text-base font-medium leading-tight text-gray-500"
-          >
-            Отклики
-          </a>
-        </div>
-      </nav>
-      <div className=" w-full">
-        <div className=" w-full">
-          <div className=" mb-6 flex flex-row justify-between">
-            <p className=" font-['Roboto'] text-3xl font-normal leading-normal tracking-wide text-white">
-              Профиль
-            </p>
-            <button className="flex h-14 w-44 items-center justify-center gap-5 rounded-lg bg-neutral-600 px-6 py-4 font-['Roboto'] text-base font-normal leading-normal tracking-wide text-zinc-100">
-              Редактировать
-            </button>
+      <NavBar />
+      <div className=" mt-12 w-full text-onPrimary-anti-flash-withe">
+        <div className=" mb-6 flex  justify-between">
+          <div className="flex items-center">
+            <Link href="/projects">
+              <Image
+                src={arrowLeft as string}
+                alt="arrowDown"
+                width={24}
+                height={24}
+              />
+            </Link>
+            <p className="ml-4 text-3xl">Профиль</p>
           </div>
-          <div className=" mb-6 flex flex-row gap-6 rounded-2xl border border-hidden bg-zinc-800 p-12">
-            <div>
-              {user?.image && user?.name ? (
-                <Image
-                  src={user.image}
-                  alt={user.name}
-                  width={208}
-                  height={208}
-                  className="rounded-full"
-                />
-              ) : (
-                <div className="h-52 w-52 rounded-full bg-zinc-100" />
-              )}
+          <MainButtonLink
+            text="Редактировать"
+            path={`/pages/user/${user.id}/settings`}
+            target="_self"
+          />
+        </div>
+        <SectionWrapper className=" mb-6 flex  gap-6">
+          <div>
+            <ProfileImage
+              imageSrc={user.image}
+              alt={user.name ?? "Не найден"}
+            ></ProfileImage>
+          </div>
+          <div className=" flex flex-col gap-5">
+            <div className=" flex items-center gap-5">
+              <p className="text-3xl font-bold ">{user.name}</p>
+              <Badge text={user.profession ?? ""} />
             </div>
-            <div>
-              <div className=" flex flex-row gap-5">
-                <p className="font-['Roboto'] text-2xl font-normal leading-normal tracking-wide text-white">
-                  {user?.name}
-                </p>
-                <div className="inline-flex items-center justify-end gap-2.5 rounded-xl border border-white px-3 py-0.5">
-                  <p className="text-center font-['Roboto'] text-xl font-normal leading-normal tracking-wide text-white">
-                    {user?.profession}
-                  </p>
+            <p>Город: {user.city}</p>
+            <p>Возраст: {user.age}</p>
+            <p>Владение языками: {user.languages}</p>
+            <div className=" flex flex-col gap-6">
+              <p className="font-bold">Контакты</p>
+              <div className="flex">
+                <div className=" mr-6 flex ">
+                  <p>{user.email}</p>
                 </div>
-              </div>
-              <p className="font-['Roboto'] text-lg font-normal leading-normal tracking-wide text-white">
-                Город: {user?.city}
-              </p>
-              <p className="font-['Roboto'] text-lg font-normal leading-normal tracking-wide text-white">
-                Возраст: {user?.age}
-              </p>
-              <p className="font-['Roboto'] text-lg font-normal leading-normal tracking-wide text-white">
-                Владение языками: {user?.languages}
-              </p>
-              <div className=" flex flex-col">
-                <p className="font-['Roboto'] text-xl font-normal leading-normal tracking-wide text-white">
-                  Контакты
-                </p>
-                <div className=" flex flex-row">
-                  <div className=" mr-6 flex flex-row">
-                    <p className="font-['Roboto'] text-xl font-normal leading-normal tracking-wide text-white">
-                      {user?.email}
-                    </p>
-                  </div>
-                  <div className=" flex flex-row">
-                    <p className="font-['Roboto'] text-xl font-normal leading-normal tracking-wide text-white">
-                      {user?.telegram}
-                    </p>
-                  </div>
+                <div className="flex">
+                  <p>{user.telegram}</p>
                 </div>
               </div>
             </div>
           </div>
-          <div className="mb-6 inline-flex w-full flex-col gap-6 rounded-2xl border border-hidden bg-zinc-800 p-12">
-            <p className="font-['Roboto'] text-3xl font-normal leading-normal tracking-wide text-zinc-100">
-              Обо мне
-            </p>
-            <p className=" w-3/4 font-['Roboto'] text-base font-normal leading-normal tracking-wide text-zinc-100">
-              {user?.description}
-            </p>
-          </div>
-        </div>
+        </SectionWrapper>
+        <SectionWrapper className="mb-6 flex w-full flex-col gap-6">
+          <p className="text-xl font-bold">Обо мне</p>
+          <p className=" w-3/4">{user.description}</p>
+        </SectionWrapper>
         <div>
-          <div className=" mb-6 flex flex-row justify-between">
-            <p className="font-['Roboto'] text-3xl font-normal leading-normal tracking-wide text-white">
-              Мои проекты
-            </p>
-            <button className="flex h-14 w-44 items-center justify-center gap-5 rounded-lg bg-neutral-600 px-6 py-4 font-['Roboto'] text-base font-normal leading-normal tracking-wide text-zinc-100">
-              Создать проект
-            </button>
+          <div className=" mb-6 flex  justify-between">
+            <p className="text-3xl ">Мои проекты</p>
+            <MainButtonLink
+              text="Создать проект"
+              path={`/pages/projects/create`}
+              target="_self"
+            />
           </div>
           <div className=" grid grid-cols-2 grid-rows-3 gap-6">
-            {user?.memberOfProjects.map((project) => {
+            {user.memberOfProjects.map((project) => {
               return (
-                <Link href={`/projects/${project.id}`}className=" flex flex-row gap-6 rounded-2xl border  border-hidden bg-zinc-800 p-8">
-                  <div className=" flex flex-col justify-between ">
-                    <div>
-                      <p className="font-['Inter'] text-2xl font-normal leading-7 tracking-tight text-white">
-                        {project.title}
-                      </p>
-                    </div>
-                    <div>
-                      <p className="font-['Inter'] text-base font-normal leading-tight tracking-tight text-zinc-100">
-                        Не могу вывести
-                      </p>
-                    </div>
-                    <div>
-                      <p className="font-['Inter'] text-base font-normal leading-tight tracking-tight text-zinc-100">
-                        {project.status}
-                      </p>
-                    </div>
-                  </div>
-                  <div className="flex flex-col justify-between">
-                    <div>
-                      <p className="font-['Inter'] text-base font-normal leading-tight tracking-tight text-zinc-100">
-                        Участник
-                      </p>
-                    </div>
-                    <div>
-                      <p className="font-['Inter'] text-base font-normal leading-tight tracking-tight text-zinc-100">
-                        {project.term}
-                      </p>
-                    </div>
-                  </div>
-                </Link>
+                <UserProjectCard project={project} role="Создатель"/>
+              );
+            })}
+            {user.createdProjects.map((project) => {
+              return (
+                <UserProjectCard project={project} role="Участник"/>
               );
             })}
           </div>
---
+++ src/modules/userProjectCard/UserProjectCard.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+import Link from "next/link";
+import SectionWrapper from "@/ui/sectionWrapper/SectionWrapper";
+interface Project {
+  id: string;
+  title: string;
+  description: string | null;
+  term: string;
+  status: string;
+  published: Date;
+}
+
+type Props = {
+  project: Project;
+  role: string;
+};
+
+const ProjectCard: React.FC<Props> = React.memo(({project, role}) => {
+  return (
+    <Link href={`/pages/projects/${project.id}`} className=" wflex  gap-6">
+      <SectionWrapper className="flex flex-col gap-4">
+        <div className=" flex justify-between">
+          <p className="text-xl font-bold">{project.title}</p>
+          <p className="text-sm">{role}</p>
+        </div>
+        <div>
+          <p className=" text-sm text-secondary-cadet-grey">Описание</p>
+          <p>{project.description}</p>
+        </div>
+        <p className="text-sm">Длительность: {project.term}</p>
+        <div className=" flex justify-between">
+          <p className="text-sm">Статус: {project.status}</p>
+          <p className="text-sm">{project.published.toString()}</p>
+        </div>
+      </SectionWrapper>
+    </Link>
+  );
+});
+export default ProjectCard;
