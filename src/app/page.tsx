7286e343b9b55e2eeb668eb0a1c0292e0b5bd590
Jegor Volkov <79021164+Vo7kov@users.noreply.github.com>
2024-01-20 22:07:42 +0200

    feat(TAS-174): remove pages directory (#62)
    * fix(TAS-174): add key prop
    
    * feat(TAS-174): remove app directory

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 
-import HomePage from "@/pages/homePage/HomePage";
-
 import HomeHeader from "@/components/header/homeHeader/HomeHeader";
+
 import Container from "@/ui/container/Container";
+import SecondaryButtonLink from "@/ui/secondaryButton/SecondaryButtonLink";
+import MainButtonLink from "@/ui/mainButton/MainButtonLink";
 
 const Home: React.FC = React.memo(() => {
   return (
@@ -12,7 +13,34 @@ const Home: React.FC = React.memo(() => {
 
       <main className="flex-1">
         <Container>
-          <HomePage />
+          <section className="mt-36 flex flex-col gap-12">
+            <div className="flex max-w-2xl flex-col gap-6">
+              <h1 className="text-6xl text-onPrimary-anti-flash-withe">
+                Эффективное решение
+                <br />
+                для поиска IT команд
+              </h1>
+
+              <p className="text-3xl text-onPrimary-anti-flash-withe">
+                С connectTeam вы можете легко найти команду для разработки или
+                создать свой собственный проект
+              </p>
+            </div>
+
+            <div className="flex items-center gap-8">
+              <MainButtonLink
+                text="Найти команду"
+                path="/projects"
+                target="_self"
+              />
+
+              <SecondaryButtonLink
+                text="Создать проект"
+                path="/projects/create"
+                target="_self"
+              />
+            </div>
+          </section>
         </Container>
       </main>
     </body>
