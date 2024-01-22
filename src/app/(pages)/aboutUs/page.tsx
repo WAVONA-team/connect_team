66a2059a0fb9f282b31d47340dee0b8418b52d69
Vo7kov <jegor.volkov.a@gmail.com>
2024-01-22 23:03:52 +0100

    feat(TAS-178): about us page

---
+++ src/app/(pages)/aboutUs/page.tsx
@@ -0,0 +1,139 @@
+import React from "react";
+import Image from "next/image";
+
+import BackButton from "@/ui/backButton/BackButton";
+import Container from "@/ui/container/Container";
+
+import wavonaLogo from "../../../../public/images/wavonaLogo.png";
+import Link from "next/link";
+
+const AboutUs: React.FC = React.memo(() => {
+  const members = [
+    {
+      url: `bg-[url("/images/team/Oleksii.jpg")]`,
+      name: "Алексей",
+      position: "Project Manager",
+      social: {
+        Linkedin: "https://www.linkedin.com/in/oleksii-kalinichenko-333616264",
+      },
+    },
+    {
+      url: `bg-[url("/images/team/Oleg.jpg")]`,
+      name: "Олег",
+      position: "Team Lead, Developer",
+      social: {
+        Github: "https://github.com/VofSwords",
+      },
+    },
+    {
+      url: `bg-[url("/images/team/Jegor.jpg")]`,
+      name: "Егор",
+      position: "Developer",
+      social: {
+        Github: "https://github.com/vo7kov",
+      },
+    },
+    {
+      url: `bg-[url("/images/team/Artem.jpg")]`,
+      name: "Артем",
+      position: "Developer",
+      social: {
+        Github: "https://github.com/aguilam",
+      },
+    },
+    {
+      url: `bg-[url("/images/team/Darya.jpg")]`,
+      name: "Дарья",
+      position: "Designer",
+      social: {
+        Behance: "https://www.behance.net/dashhasano662b",
+      },
+    },
+    {
+      url: `bg-[url("/images/team/Anastasiia.jpg")]`,
+      name: "Анастасия",
+      position: "Designer",
+      social: {
+        Behance: "https://www.behance.net/messhx",
+      },
+    },
+  ];
+
+  return (
+    <section className="mt-12">
+      <Container>
+        <div className="flex items-center gap-2">
+          <BackButton />
+
+          <h2 className="text-3xl text-onPrimary-anti-flash-withe">
+            Наша команда
+          </h2>
+        </div>
+
+        <div className="mt-8 flex flex-col items-center gap-6">
+          <div className="flex items-center gap-4">
+            <Image src={wavonaLogo} alt="Wavona logo" width={70} height={70} />
+
+            <p className="text-3xl font-black text-onPrimary-anti-flash-withe">
+              Wavona team
+            </p>
+          </div>
+
+          <p className="max-w-4xl text-2xl text-onPrimary-anti-flash-withe">
+            Wavona Team - команда, стоящая за разработкой этого сайта. В её
+            <br />
+            состав входят талантливые разработчики и креативные дизайнеры.
+          </p>
+        </div>
+
+        <div className="mt-12 grid grid-cols-3 gap-x-6 gap-y-12">
+          {members.map((member) => {
+            const { url, name, position, social } = member;
+
+            return (
+              <div
+                key={url}
+                className="hover:via-[rgba(37, 32, 49, 0.00)] flex flex-col items-center gap-9 rounded-2xl px-12 py-8 hover:bg-gradient-to-b hover:from-transparent hover:to-[#4F46E5]"
+              >
+                <div
+                  className={`${url} h-64 w-64 rounded-full bg-contain bg-center bg-no-repeat`}
+                />
+
+                <div className="flex flex-col items-center gap-6">
+                  <div className="flex flex-col items-center gap-4">
+                    <h2 className="text-2xl font-bold text-onPrimary-anti-flash-withe">
+                      {name}
+                    </h2>
+
+                    <p className="text-base font-bold text-onPrimary-anti-flash-withe">
+                      {position}
+                    </p>
+                  </div>
+
+                  {Object.entries(
+                    social as Record<string, string | undefined>,
+                  ).map((item) => {
+                    const [key, value] = item;
+
+                    return (
+                      <Link
+                        className="text-sm text-onPrimary-anti-flash-withe"
+                        href={value!}
+                        target="_blank"
+                        key={value}
+                      >
+                        {key}
+                      </Link>
+                    );
+                  })}
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </Container>
+    </section>
+  );
+});
+
+export default AboutUs;
