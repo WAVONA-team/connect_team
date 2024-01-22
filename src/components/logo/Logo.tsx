82d51783c3bbc62c62969ffeac28d7615371d9db
Vo7kov <jegor.volkov.a@gmail.com>
2024-01-22 21:21:45 +0100

    feat(TAS-180): change logo

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import logo from '../../../public/images/logo.svg';
+
+import logo from "../../../public/images/logo.svg";
+
 const Logo: React.FC = React.memo(() => (
-  <Link className="flex items-center gap-4" href={"/"}>
-    <Image src={logo as string} alt="logo" width={50} height={50} />
-    <p className="text-xl text-white leading-6 font-bold">ConnectTeam</p>
+  <Link href={"/"}>
+    <Image src={logo as string} alt="logo" width={185} height={50} />
   </Link>
 ));
 
