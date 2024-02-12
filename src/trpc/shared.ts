1a7193a3beeb4fdc8429a355ee5dd5d789a81c86
Jegor volkov <jegor.volkov.a@gmail.com>
2024-02-12 17:33:06 +0100

    fix(MASTER): add port

--- src/trpc/shared.ts
+++ src/trpc/shared.ts
@@ -7,7 +7,7 @@ export const transformer = superjson;
 
 function getBaseUrl() {
   if (typeof window !== "undefined") return "";
-  if (process.env.SERVER_URL) return `https://${process.env.SERVER_URL}`;
+  if (process.env.SERVER_URL) return `https://${process.env.SERVER_URL}:${process.env.PORT ?? 3000}`;
   return `http://localhost:${process.env.PORT ?? 3000}`;
 }
 
