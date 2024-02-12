acac136f8dd2f6f0125eb216feb31bd55c95c891
Jegor volkov <jegor.volkov.a@gmail.com>
2024-02-12 19:29:18 +0100

    add support for google avatars

--- next.config.js
+++ next.config.js
@@ -14,6 +14,12 @@ const config = {
         port: "",
         pathname: "/u/**",
       },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        port: "",
+        pathname: "/u/**",
+      },
     ],
   },
 };
